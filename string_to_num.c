11a65692f7fc1bb6ff821d3f9fb3f9d0cfcf49d5
tarun <bagewaditarun@gmail.com>
2024-01-06 11:43:23 +0530

    added the function the converts string to long long int

--- string_to_num.c
+++ string_to_num.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+long long int string_to_num(char str[20])
+{
+    long long int num = 0;
+    int len = strlen(str) - 1;
+    for(int i = len; i>=0; i--)
+        num = num * 10 + (str[i] - 48);
+
+    return num;
+}
 
 int main()
 {
-    char str[10];
-    printf("Enter the string of numbers");
+    char str[20];
+    printf("Enter the string of numbers (No of digits<=19)");
     scanf("%s",str);
 
-    printf("Entered String: %s",str);
-    
+    long long int num = string_to_num(str);
+    printf("IN MAIN\n");
+    printf("long long int = %llu",num);
+
 }
+
+
