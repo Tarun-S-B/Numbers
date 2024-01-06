fb5ccc39d8a7daa44ab9b8b5db6a22c1ca49b493
tarun <bagewaditarun@gmail.com>
2024-01-06 12:16:49 +0530

    added the string to int array function

--- string_to_num.c
+++ string_to_num.c
@@ -13,15 +13,40 @@ long long int string_to_num(char str[20])
     return num;
 }
 
+
+int * string_to_array(char str[])
+{
+    int n = strlen(str);
+    int *a = (int *) malloc (sizeof(int) * n);
+
+    for(int i=0; str[i]; i++)
+        a[i] = str[i] - 48;
+
+    return a;
+}
+
+
+
 int main()
 {
-    char str[20];
-    printf("Enter the string of numbers (No of digits<=19)");
+    // char str[20];
+    // printf("Enter the string of numbers (No of digits<=19)");
+    // scanf("%s",str);
+
+    // long long int num = string_to_num(str);
+    // printf("IN MAIN\n");
+    // printf("long long int = %llu",num);
+
+    char str[200];
+    printf("Enter a string of numbers: ");
     scanf("%s",str);
 
-    long long int num = string_to_num(str);
-    printf("IN MAIN\n");
-    printf("long long int = %llu",num);
+    int *num_array = string_to_array(str);
+    int n = strlen(str);
+
+    for(int i=0; i<n; i++)
+        printf("%d ",num_array[i]);
+    
 
 }
 
