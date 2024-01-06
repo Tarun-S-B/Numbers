2cfae99003c22b8024616199af42764b643c0c27
tarun <bagewaditarun@gmail.com>
2024-01-06 12:24:30 +0530

    added the verification case for invalid data

--- string_to_num.c
+++ string_to_num.c
@@ -3,24 +3,46 @@
 #include<string.h>
 
 
+//INPUT: takes an array of characters of size 20 (including NULL character)
+//OUTPUT: returns a long long int which is formed from the input string
 long long int string_to_num(char str[20])
 {
     long long int num = 0;
+    int rem;
     int len = strlen(str) - 1;
     for(int i = len; i>=0; i--)
-        num = num * 10 + (str[i] - 48);
+    {
+        rem = str[i] - 48;
+        if(rem>=9 || rem<=0)
+        {
+            printf("Invalid string\n");
+            return 0;
+        }
+        num = num * 10 + rem;
+    }
 
     return num;
 }
 
 
+//INPUT: takes an string of numbers of any size
+//OUTPUT: the address of integer array containing the digits of string
 int * string_to_array(char str[])
 {
     int n = strlen(str);
+    int x;
     int *a = (int *) malloc (sizeof(int) * n);
 
     for(int i=0; str[i]; i++)
-        a[i] = str[i] - 48;
+    {
+        x = str[i] - 48;
+        if(x>=9 || x<=0)
+        {
+            printf("Invalid input\n");
+            return NULL;
+        }
+        a[i] = x;
+    }
 
     return a;
 }
